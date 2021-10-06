1a05925b1029c5717198ed78cd11a8319ddb6a90
Esneyder Saavedra <esneydersa@hotmail.com>
2021-10-06 06:41:50 -0500

    implementando componenetes html base

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.resolve(__dirname,'public')))
+
+app.get('/',(req,res)=>{
+	let htmlPath = path.join(__dirname, '/views/index.html');
+    res.sendFile(htmlPath);
+});
+
+
+app.listen(port, () => console.log('listen in port 3000'))
