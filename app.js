bb6620892c8641b87b82755c2d58835a7238fd38
Esneyder Saavedra <esneydersa@hotmail.com>
2021-10-15 07:14:13 -0500

    Agregando extructura html basica de register y login

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.resolve(__dirname,'public')))
 
 app.get('/',(req,res)=>{
-	let htmlPath = path.join(__dirname, '/views/index.html');
+	let htmlPath = path.join(__dirname, './views/index.html');
     res.sendFile(htmlPath);
 });
+
+app.get('/register',(req,res)=>{
+	let htmlPath = path.join(__dirname, './views/register.html');
+    res.sendFile(htmlPath);
+});
+app.get('/login',(req,res)=>{
+	let htmlPath = path.join(__dirname, './views/login.html');
+    res.sendFile(htmlPath);
+});
+
 
 
 app.listen(port, () => console.log('listen in port 3000'))
